514ed23f2490a001447ae318b151cc03b19fb6ea
KILINC Bayram (SNCF VOYAGEURS / DIRECTION DE LIGNE PARIS GRAND EST / EQUIPE TRAIN PARIS EST 4) <8103509m@commun.ad.sncf.fr>
2023-08-01 13:44:02 +0200

    mise en place typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT, // Assurez-vous de convertir le port en un nombre
+      username: String(process.env.DB_USERNAME),
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log('TypeORM config:', {
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
+  }
+}
